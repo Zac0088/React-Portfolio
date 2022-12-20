f2e1003bfffbcbfa679cca165771a9a65c2ab373
Zac Smart <zac_chaz@outlook.com>
2022-12-20 19:47:01 +1100

    was having compiling issues via syntax error on public folder

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,14 @@
 import React from 'react';
+import './Home.css'
 
-export default function About() {
+export default function Home() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+       Hello there!! 
+       Thanks for dropping by my Portfolio.
+       Im Zac Junior Web developer studying Full stack web development via Sydney University
       </p>
     </div>
   );
