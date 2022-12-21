276a025fd8301cad4a1f965a847b11ed1ceba185
Zac Smart <zac_chaz@outlook.com>
2022-12-21 13:46:00 +1100

    added info to about me section added contact me section

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 export default function Contact() {
+  const handleSubmit = (event) => {
+  }
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>
-       
-      </p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='Name'>Name:</label><br />
+        <input type="text" id='name' /><br />
+        <label htmlFor='email'>Email:</label><br />
+        <input type="email" id="eamil" /><br />
+        <label htmlFor='message'>Message:</label><br />
+        <textarea id="message" />
+        <button type='Submit'>Send</button>
+      </form>
     </div>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import profilepic from '../../Assets/profilepic.jpg'
 import './Home.css'
 
 export default function Home() {
   return (
-    <section className="about-me-container">
-    <h2 id="about-me">About Me</h2>
-    <div className='image-container'>
-      <img src={require(`../../Assets/hero.png`).default}/>
+    <div>
+      <h1 className='topText'>About Me</h1>
+      <div className='aboutTextStyle'>
+        <h2>
+          Hello, Welcome to my Portfolio!!
+        </h2>
+        <h2>
+          My name is Zac Smart
+          <img src={profilepic} alt="selfie" width="190" height="250" className='selfie'/>
+        </h2>
+        <p>I am a junior software engineer living in Melbourne Victoria studying Full Stack Web Development through Syndey University in sunny Austrtalia.
+          I started this adventure as a fresh start for myself after having various jobs in the past and never really enjoying my work.
+           After the very first class i was hooked on the endless possibilites that being a web developer offered me.
+           In my Spare time i love  playing video games, photograpy and Basketball, i am also a 90s Hip Hop addict.
+           Please look through the various applications I have worked on over my course work, but also watch this space as my journey continues outside of my current course and I Develop as a Full stack web Developer.
+          Feel free to reach out to me im looking forward to hearing from you!!
+          Regards 
+          Zac Smart
+
+
+        </p>
       </div>
-    </section>
+    </div>
+    
   );
 }
